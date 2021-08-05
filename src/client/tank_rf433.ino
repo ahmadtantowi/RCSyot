1e061a4962c2b0e3aecf6ecc5c8386179c13ab0d
Ahmad <ahmadtantowi@outlook.com>
2021-08-05 09:49:54 +0700

    Complete client sketch with driver MX1508

--- src/client/tank_rf433.ino
+++ src/client/tank_rf433.ino
@@ -1,8 +1,46 @@
 #include <RH_ASK.h>
 #include <SPI.h>
+#include <MX1508.h>
 
 // uncomment to enable print out debug to uart
 // #define COM_DEBUG
+
+// RF433 pin
+#define RF_SPEED    2000
+#define RF_RX       4
+#define RF_TX       7
+#define RF_PTT      8
+
+// MX1508 pin
+#define MOTOR_A1    11
+#define MOTOR_A2    6
+#define MOTOR_B1    3
+#define MOTOR_B2    5
+
+// buzzer pin
+#define BUZZER  2
+
+// PS 2 controller button
+#define PSB_SELECT      0x0001
+#define PSB_L3          0x0002
+#define PSB_R3          0x0004
+#define PSB_START       0x0008
+#define PSB_PAD_UP      0x0010
+#define PSB_PAD_RIGHT   0x0020
+#define PSB_PAD_DOWN    0x0040
+#define PSB_PAD_LEFT    0x0080
+#define PSB_L2          0x0100
+#define PSB_R2          0x0200
+#define PSB_L1          0x0400
+#define PSB_R1          0x0800
+#define PSB_GREEN       0x1000
+#define PSB_RED         0x2000
+#define PSB_BLUE        0x4000
+#define PSB_PINK        0x8000
+#define PSB_TRIANGLE    0x1000
+#define PSB_CIRCLE      0x2000
+#define PSB_CROSS       0x4000
+#define PSB_SQUARE      0x8000
 
 // data type to hold PS 2 value
 struct PS2_DATA {
@@ -13,15 +51,21 @@ struct PS2_DATA {
   byte Right_X;
 };
 
-RH_ASK _rf_driver;
+RH_ASK _rf_driver(RF_SPEED, RF_RX, RF_TX, RF_PTT);
+MX1508 _motor_a(MOTOR_A1, MOTOR_A2, FAST_DECAY, PWM_2PIN);
+MX1508 _motor_b(MOTOR_B1, MOTOR_B2, FAST_DECAY, PWM_2PIN);
+
 PS2_DATA *_ps2_data;
+int _dpad_pwm = 127;
 
 void setup() {
 #ifdef COM_DEBUG
     Serial.begin(57600);
 #endif
 
-    _rf_driver.init();
+    if (_rf_driver.init()) {
+        tone(BUZZER, 500, 100);
+    }
 }
 
 void loop() {
@@ -42,6 +86,61 @@ void loop() {
         Serial.print(" : ");
         Serial.print(_ps2_data->Right_Y, HEX);
         Serial.println();
-    }
 #endif
+        
+        int left_analog = map(_ps2_data->Left_Y, 0, 255, 767, -767);
+        int right_analog = map(_ps2_data->Right_Y, 0, 255, 767, -767);
+        
+        // drive with analog
+        if (left_analog == 28) { // analog on center
+            _motor_a.stopMotor();
+        }
+        else {
+            _motor_a.motorGo(left_analog);
+        }
+        if (right_analog == 28) { // analog on center
+            _motor_b.stopMotor();
+        }
+        else {
+            _motor_b.motorGo(right_analog);
+        }
+
+        // drive with dpad
+        if (IsPressed(PSB_PAD_UP)) {
+            _motor_a.motorGo(_dpad_pwm);
+            _motor_b.motorGo(_dpad_pwm);
+        }
+        else if (IsPressed(PSB_PAD_DOWN)) {
+            _motor_a.motorGo(-_dpad_pwm);
+            _motor_b.motorGo(-_dpad_pwm);
+        }
+        else if (IsPressed(PSB_PAD_LEFT)) {
+            _motor_a.motorGo(-_dpad_pwm);
+            _motor_b.motorGo(_dpad_pwm);
+        }
+        else if (IsPressed(PSB_PAD_RIGHT)) {
+            _motor_a.motorGo(_dpad_pwm);
+            _motor_b.motorGo(-_dpad_pwm);
+        }
+
+        // stop when square pressed
+        if (IsPressed(PSB_SQUARE)) {
+            _motor_a.stopMotor();
+            _motor_b.stopMotor();
+        }
+
+        // increase/decrease pwm speed with L1/R1
+        if (IsPressed(PSB_L1) && _dpad_pwm >= 20) {
+            _dpad_pwm -= 3;
+            tone(BUZZER, 250, 50);
+        }
+        if (IsPressed(PSB_R1) && _dpad_pwm <= 767) {
+            _dpad_pwm += 3;
+            tone(BUZZER, 500, 50);
+        }
+    }
+}
+
+boolean IsPressed(uint16_t ps2_button) {
+    return ((_ps2_data->Button & ps2_button) > 0);
 }
